8a79c12a303a519fd2812ee24e23fff383b5db2e
Tomus <tomuso.o@gmail.com>
2018-09-10 22:14:51 +0200

    Removed X node, added Scene Duplicator

---
+++ Assets/Editor/SceneSetupEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneSetup))]
+public class SceneSetupEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		SceneSetup sceneSetup = (SceneSetup)target;
+		if(GUILayout.Button("Copy Scene"))
+		{
+			sceneSetup.NewScene();
+		}
+	}
+}
---
+++ Assets/Scripts/SceneSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SceneSetup : MonoBehaviour {
+
+	int number = 0;
+
+	public void NewScene()
+	{
+		FileUtil.CopyFileOrDirectory("Assets/Scenes/Scene.unity", "Assets/Scenes/Scene" + number + ".unity");
+		AssetDatabase.Refresh();
+		number++;
+	}
+
+}
