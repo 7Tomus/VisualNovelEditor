345505e49d11fd303b81e684baf54b722085ca17
Tomus <tomuso.o@gmail.com>
2018-10-11 23:22:46 +0200

    Editor GUI buttons - wip

--- Assets/Editor/SceneSetupEditor.cs
+++ Assets/Editor/SceneSetupEditor.cs
@@ -1,13 +1,44 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(SceneSetup))]
 public class SceneSetupEditor : Editor {
 
 	public override void OnInspectorGUI()
 	{
-		DrawDefaultInspector();
-		
+
+		List<int> linklist = new List<int>();
+		linklist.Add(1);
+		linklist.Add(2);
+		linklist.Add(3);
+
+		List<int> nextlinklist = new List<int>();
+		nextlinklist.Add(1);
+
+		GUILayout.BeginHorizontal();
+		if(GUILayout.Button("Previous Scene"))
+		{
+
+		}
+		GUILayout.BeginVertical();
+		foreach(int i in linklist)
+		{
+			if(GUILayout.Button("Next Scene" + i))
+			{
+
+			}
+		}
+		GUILayout.EndVertical();
+		GUILayout.EndHorizontal();
+		foreach(int i in nextlinklist)
+		{
+			if(GUILayout.Button("New Scene" + i))
+			{
+				nextlinklist.Add(i + 1); //TODO
+			}
+		}
+		GUILayout.Space(20);
 		SceneSetup sceneSetup = (SceneSetup)target;
 		if(GUILayout.Button("New Scene"))
 		{
