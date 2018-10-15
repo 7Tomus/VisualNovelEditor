7d07ad988fd66fbb52f85114c72384345e95097e
Tomus <tomuso.o@gmail.com>
2018-10-15 21:19:13 +0200

    Create New Scene method

--- Assets/Scripts/SceneLinker.cs
+++ Assets/Scripts/SceneLinker.cs
@@ -10,27 +10,32 @@ public class SceneLinker : ScriptableObject
 	#endregion
 
 	#region PublicMethods
-	public void CreateNewScene(int currentSceneNumber)
+	public int CreateNewScene(int currentSceneNumber)
 	{
+		int freshSceneNumber;
+		SceneCounter sceneCounter = Resources.Load<SceneCounter>("SceneCounter");
+		freshSceneNumber = sceneCounter.GetFreshSceneNumber();
+
 		SceneLink currentSceneLinks = new SceneLink();
 		SceneLink nextSceneLinks = new SceneLink();
-		SceneCounter sceneCounter = Resources.Load<SceneCounter>("SceneCounter");
 
 		if(sceneLinkChain.TryGetValue(currentSceneNumber, out currentSceneLinks))
 		{
-			//FILL CURRENT SCENE LINK AND NEXT SCENE LINK
-			int freshSceneNumber = sceneCounter.GetFreshSceneNumber();
-
-			SceneLink sceneLink = new SceneLink();
-
-			sceneLink.nextScenes.Add(sceneCounter.GetFreshSceneNumber());
-			sceneLink.previousScenes.Add(currentSceneNumber - 1);
-			sceneLinkChain.Add(currentSceneNumber, sceneLink);
+			currentSceneLinks.nextScenes.Add(freshSceneNumber);
+			sceneLinkChain.Remove(currentSceneNumber);
+			sceneLinkChain.Add(currentSceneNumber, currentSceneLinks);
+			nextSceneLinks.previousScenes.Add(currentSceneNumber);
+			sceneLinkChain.Add(freshSceneNumber, nextSceneLinks);
 		}
 		else
 		{
 			Debug.LogWarning("Current Scene is not inside scene container (◕︿◕✿)");
+			currentSceneLinks.nextScenes.Add(freshSceneNumber);
+			sceneLinkChain.Add(0, currentSceneLinks);
+			nextSceneLinks.previousScenes.Add(0);
+			sceneLinkChain.Add(freshSceneNumber, nextSceneLinks);
 		}
+		return freshSceneNumber;
 	}
 	#endregion
 }
