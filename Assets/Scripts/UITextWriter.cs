7dbd98a2f893e9f7db75149e4048de4eb5ca98c2
Tomus <tomuso.o@gmail.com>
2018-12-13 22:47:06 +0100

    Next text by space

--- Assets/Scripts/UITextWriter.cs
+++ Assets/Scripts/UITextWriter.cs
@@ -7,30 +7,63 @@ public class UITextWriter : MonoBehaviour
 {
 
 	#region Variables
+	public float textSpeed = 0.05f;
+	[Multiline] public string[] paragraphs;
 	private TextMeshProUGUI mainText;
 	private string originalText;
-	public float textSpeed = 0.05f;
+	private int currentParagraph = 0;
+	private IEnumerator writeCoroutine;
+	int currentChar = 0; 
 	#endregion
 
 	private void Awake()
 	{
 		mainText = GetComponent<TextMeshProUGUI>();
-		originalText = mainText.text;
-		mainText.text = "";
+		writeCoroutine = WriteText();
 	}
 
 	void Start()
     {
-		StartCoroutine(writeText());
-    }
+		NextText();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			NextText();
+		}
+		if(Input.GetKeyDown(KeyCode.K))
+		{
+			StopCoroutine(writeCoroutine);
+		}
+	}
+
+	#region Methods
+	private void NextText()
+	{
+		StopCoroutine(writeCoroutine);
+		writeCoroutine = null;
+		writeCoroutine = WriteText();
+		currentChar = 0;
+		if(paragraphs.Length > currentParagraph)
+		{
+			originalText = paragraphs[currentParagraph];
+			mainText.text = "";
+			currentParagraph++;
+			StartCoroutine(writeCoroutine);
+		}
+	}
+	#endregion
 
 	#region Coroutines
-	IEnumerator writeText()
+	IEnumerator WriteText()
 	{
-		for(int i = 0; i < originalText.Length; i++)
+		Debug.Log("write coroutine started");
+		for(currentChar = 0; currentChar < originalText.Length; currentChar++)
 		{
 			yield return new WaitForSeconds(textSpeed);
-			mainText.text = mainText.text + originalText[i];
+			mainText.text = mainText.text + originalText[currentChar];
 		}
 	}
 	#endregion
